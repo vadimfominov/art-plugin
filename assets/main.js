a9856683288cd788d18a922d114b930642dd1247
Vadim Fominov <mail@fominov.agency>
2026-05-04 10:47:39 +0300

    added new block: block-for-msk

--- assets/main.js
+++ assets/main.js
@@ -17,8 +17,10 @@ window.addEventListener('load', function () {
 			// Разбираем URL
 			const url = new URL(originalHref);
 
-			const domain = url.host; // Получаем домен 
-			const pathname = url.pathname; // Получаем хвост после домена
+			console.log(url);
+
+			const domain = url.host; // Получаем домен (http://localhost:8888)
+			const pathname = url.pathname; // Получаем хвост после домена (/art.loc/professions/)
 			const protocol = url.protocol;
 
 			// Проверяем, есть ли уже контейнер с ссылками для этой ссылки
@@ -42,15 +44,17 @@ window.addEventListener('load', function () {
 
 				// Создаём первую ссылку (домен)
 				const domainLink = document.createElement('a');
-				// domainLink.href = protocol + '//' + domain + '/professions/';
-				domainLink.href = protocol + '//' + domain + pathname;
+				domainLink.href = protocol + '//' + domain + '/professions-spb/';
+				// domainLink.href = protocol + '//' + domain + pathname;
 				domainLink.textContent = 'В Санкт-Петербурге';
+				domainLink.target = '_blank';
 
 				// Создаём вторую ссылку (хвост)
 				const pathLink = document.createElement('a');
-				// pathLink.href = protocol + '//' + domain + pathname;
-				pathLink.href = protocol + '//' + 'msk.' + domain + pathname;
+				pathLink.href = protocol + '//' + domain + pathname;
+				// pathLink.href = protocol + '//' + 'msk.' + domain + pathname;
 				pathLink.textContent = 'В Москве';
+				pathLink.target = '_blank';
 
 				// Добавляем ссылки в контейнер
 				linksContainer.appendChild(domainLink);
@@ -3652,7 +3656,5 @@ window.addEventListener('load', function () {
 			modalContent.classList.remove('active');
 		});
 	});
-
-
 
 });
