724850fa3752ddf0a0fda805c6a7a9acb1bcbfc9
Vadim Fominov <mail@fominov.agency>
2026-08-24 12:02:00 +0300

    updated card

--- assets/main.js
+++ assets/main.js
@@ -1537,7 +1537,7 @@ window.addEventListener('load', function () {
 			// 30366 - Родительская среда (30366 для dev и 34004 для prod) 
 			// 48 - Профтестирование (48 для DEV и 2363 для prod)
 
-			const allowedPagesFilter = ["33080", "32894", "44", "46", "788", "50", "1190", "34004", "2363"];
+			const allowedPagesFilter = ["42", "32894", "44", "46", "788", "50", "1190", "34004", "2363"];
 
 			// Проверяем, находится ли текущая страница в списке разрешённых
 			if (allowedPagesFilter.includes(CURRENT_PAGE)) {
@@ -1558,7 +1558,7 @@ window.addEventListener('load', function () {
 	};
 
 	const pageGroups = {
-		"group1": ["33080", "32894", "44", "46", "788"], 	// Группа для art-community, career-camp и других (для dev 30376 и для прод 42 на проде на test page 33080)
+		"group1": ["42", "32894", "44", "46", "788"], 	// Группа для art-community, career-camp и других (для dev 30376 и для прод 42 на проде на test page 33080)
 		"group2": ["50", "1190", "34004"],           	// Группа для psychologist, skills-courses и parent-wednesdays (30366 для dev и 34004 для prod) 
 		"group3": ["2363"]                 					// Группа для proficiency-testing  "48" - для DEV и 2363 для prod
 	};
