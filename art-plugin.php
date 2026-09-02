9bcf408ef298435ac5cc6ec393607190e5b9c63e
Vadim Fominov <mail@fominov.agency>
2026-09-02 10:41:17 +0300

    updated style

--- art-plugin.php
+++ art-plugin.php
@@ -62,13 +62,7 @@ function fv_block_assets()
 		);
 	}
 
-	wp_enqueue_style(
-		'main-msk',
-		plugin_dir_url(__FILE__) . 'assets/main-msk.css',
-		[],
-		filemtime(dirname(__FILE__) . '/assets/main-msk.css'),
-		'screen'
-	);
+	
 
 	wp_enqueue_style(
 		'main-art',
@@ -86,6 +80,14 @@ function fv_block_assets()
 			'screen'
 		);
 	}
+
+	wp_enqueue_style(
+		'main-msk',
+		plugin_dir_url(__FILE__) . 'assets/main-msk.css',
+		[],
+		filemtime(dirname(__FILE__) . '/assets/main-msk.css'),
+		'screen'
+	);
 
 
 	// // Проверяем, включен ли мультисайт
