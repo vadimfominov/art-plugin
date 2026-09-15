480612d4d3ae9f24143740c5cbb465df0756932b
Vadim Fominov <mail@fominov.agency>
2026-09-15 20:12:26 +0300

    updated js

--- assets/main.js
+++ assets/main.js
@@ -1506,7 +1506,7 @@ window.addEventListener('load', function () {
 
 			// !! 30556 заменить на 42 перед пушем
 
-			const allowedPagesFilter = ["42", "32894", "44", "46", "788", "50", "1190", "30366", "2363"];
+			const allowedPagesFilter = ["42", "32894", "44", "46", "788", "50", "1190", "34004", "2363"];
 
 			// Проверяем, находится ли текущая страница в списке разрешённых
 			if (allowedPagesFilter.includes(CURRENT_PAGE)) {
@@ -1528,7 +1528,7 @@ window.addEventListener('load', function () {
 
 	const pageGroups = {
 		"group1": ["42", "32894", "44", "46", "788"], 	// Группа для art-community, career-camp и других (для dev 30376 и для прод 42 на проде на test page 33080)
-		"group2": ["50", "1190", "30366"],           	// Группа для psychologist, skills-courses и parent-wednesdays (30366 для dev и 34004 для prod) 
+		"group2": ["50", "1190", "34004"],           	// Группа для psychologist, skills-courses и parent-wednesdays (30366 для dev и 34004 для prod) 
 		"group3": ["2363"]                 					// Группа для proficiency-testing  "48" - для DEV и 2363 для prod
 	};
 
@@ -1638,9 +1638,6 @@ window.addEventListener('load', function () {
 					placeTitle,
 					titleCount
 				} = post;
-
-				console.log(post);
-				
 
 				if (type === 'parent-wednesdays') {
 
